42c548947340e9fa9bfc48a5ad3bde84b87c4a42
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-27 14:53:30 +0300

    Section 2 Get & display Catregories

---
+++ eComStoreWeb/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using eComStoreWeb.Data;
+using eComStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eComStoreWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
