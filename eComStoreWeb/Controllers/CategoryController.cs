ea6265d6b10838d38ea85f5e78c1ca34bf358b74
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-27 18:36:49 +0300

    Section 2 Edit Category

--- eComStoreWeb/Controllers/CategoryController.cs
+++ eComStoreWeb/Controllers/CategoryController.cs
@@ -16,7 +16,33 @@ namespace eComStoreWeb.Controllers
             IEnumerable<Category> obj = _db.Categories;
             return View(obj);
         }
-       
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var objFromDb = _db.Categories.Find(id);
+
+            if (objFromDb == null) return NotFound();
+
+            return View(objFromDb);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot be the same as the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
         //Get
         public IActionResult Create()
         {
