b49579d14b797c79e4446d80bc3432eb776971d9
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-27 23:48:18 +0300

    Section 5 Creating CategoryRepository and ICategoryRepository

---
+++ eComStore.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using eComStore.DataAccess.Data;
+using eComStore.DataAccess.Repository.IRepository;
+using eComStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComStore.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db):base(db)
+        {
+            _db = db;
+        }
+    
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
