9b446334c908ed23def2976b6b585a223fe59be2
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-27 20:07:39 +0300

    Section 2 Implementing Toasters

--- eComStoreWeb/Controllers/CategoryController.cs
+++ eComStoreWeb/Controllers/CategoryController.cs
@@ -37,6 +37,7 @@ namespace eComStoreWeb.Controllers
 
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully!";
             return RedirectToAction("Index");
         }
         //Get
@@ -62,6 +63,7 @@ namespace eComStoreWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -84,6 +86,7 @@ namespace eComStoreWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully!";
                 return RedirectToAction("Index");
             }
             return View(obj);
