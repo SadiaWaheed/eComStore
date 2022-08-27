98752736013fedd3bbbe2f6f09165079c81ff5ee
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-27 14:28:51 +0300

    Add project files.

---
+++ eComStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eComStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
