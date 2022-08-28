5442f87cb214b8a2527926b42e438daba8fc6891
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-28 14:33:21 +0300

    Section 6 Product Table Created

--- eComStore.DataAccess/Data/ApplicationDbContext.cs
+++ eComStore.DataAccess/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace eComStore.DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<CoverType> CoverTypes { get; set; }    
+        public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }    
     }
 }
