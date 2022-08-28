275c366e75c02d3a75bbb5e0f7f59686f56e138f
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-28 14:03:44 +0300

    Section 5 Implementing CoverType, CoverTyprRepository and ICoverTypeRepository

--- eComStore.DataAccess/Data/ApplicationDbContext.cs
+++ eComStore.DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace eComStore.DataAccess.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }    
     }
 }
---
+++ eComStore.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,27 @@
+﻿using eComStore.DataAccess.Data;
+using eComStore.DataAccess.Repository.IRepository;
+using eComStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComStore.DataAccess.Repository
+{
+    public class CoverTypeRepository : Repository<CoverType>, ICoverTypeRepository
+    {
+        private ApplicationDbContext _db;
+        public CoverTypeRepository(ApplicationDbContext db):base(db)    
+        {
+            _db = db;
+        }
+    
+        public void Update(CoverType obj)
+        {
+            _db.Update(obj);
+        }
+
+    }
+}
