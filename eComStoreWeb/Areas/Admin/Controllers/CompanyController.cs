0edb42e7fe9b60766f162780343ed6a8426adb74
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-30 19:51:23 +0300

    Section 8 Display Companies

---
+++ eComStoreWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,32 @@
+﻿using eComStore.DataAccess.Repository.IRepository;
+using eComStore.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eComStore.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CompanyController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var companyList = _unitOfWork.Company.GetAll(); 
+            return Json(new { data = companyList});
+
+        }
+        #endregion
+    }
+}
+
+
---
+++ eComStoreWeb/wwwroot/js/company.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tableData').DataTable({
+        "ajax": {
+            "url": "/Admin/Company/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "streetAdress", "width": "15%" },
+            { "data": "city", "width": "15%" },
+            { "data": "state", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" }
+        ]
+    });
+} 
