ea40ffab62c51d8d25a130abdc85e74263656ae3
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-30 22:14:46 +0300

    Section 8 Register User with Roles

--- eComStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ eComStoreWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -17,7 +17,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
 
@@ -109,6 +111,9 @@ namespace eComStore.Web.Areas.Identity.Pages.Account
             public string? State { get; set; }
             public string? PostalCode { get; set; }
             public string? PhoneNumber { get; set; }
+            public string? Role { get; set; }
+            [ValidateNever]
+            public IEnumerable<SelectListItem> RoleList { get; set; }
         }
 
 
@@ -123,6 +128,14 @@ namespace eComStore.Web.Areas.Identity.Pages.Account
             }
             ReturnUrl = returnUrl;
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            Input = new InputModel()
+            {
+                RoleList = _roleManager.Roles.Select(i => i.Name).Select(i => new SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
@@ -147,6 +160,9 @@ namespace eComStore.Web.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    if (Input.Role == null) await _userManager.AddToRoleAsync(user, SD.Role_User_Indi);
+                    else await _userManager.AddToRoleAsync(user, Input.Role);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
@@ -184,7 +200,7 @@ namespace eComStore.Web.Areas.Identity.Pages.Account
         {
             try
             {
-                return Activator.CreateInstance <ApplicationUser>();
+                return Activator.CreateInstance<ApplicationUser>();
             }
             catch
             {
