a355a4f85caac0bc2d8a7ae3e05e47be9e8d3798
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-30 18:16:41 +0300

    Section 8 Creating company repository and unit of work

---
+++ eComStore.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,24 @@
+﻿using eComStore.DataAccess.Data;
+using eComStore.DataAccess.Repository.IRepository;
+using eComStore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eComStore.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private ApplicationDbContext _db;
+        public CompanyRepository(ApplicationDbContext db):base(db)
+        {
+            _db= db;
+        }
+        public void Update(Company obj)
+        {
+            _db.Update(obj);
+        }
+    }
+}
