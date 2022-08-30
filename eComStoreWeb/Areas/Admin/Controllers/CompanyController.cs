d114b1794931b9ee7b47764c8b75b45440c92392
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-30 20:29:04 +0300

    Section 8 Implement Add Update and Delete on company

--- eComStoreWeb/Areas/Admin/Controllers/CompanyController.cs
+++ eComStoreWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -8,14 +8,53 @@ namespace eComStore.Web.Areas.Admin.Controllers
     public class CompanyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public CompanyController(IUnitOfWork unitOfWork)
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public CompanyController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+        //Get
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new();
+            if (id == null || id == 0)
+            {
+                //Create New Product
+                return View(company);
+            }
+            else
+            {
+                //Edit Existing Product
+                company = _unitOfWork.Company.GetFirstOrDefault(i => i.Id == id);
+                return View(company);
+            }
+        }
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                }
+
+                _unitOfWork.Save();
+                TempData["success"] = "Product created successfully!";
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
         #region API Calls
         [HttpGet]
@@ -24,6 +63,21 @@ namespace eComStore.Web.Areas.Admin.Controllers
             var companyList = _unitOfWork.Company.GetAll(); 
             return Json(new { data = companyList});
 
+        }
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Company.GetFirstOrDefault(i => i.Id == id);
+
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete successful" });
         }
         #endregion
     }
