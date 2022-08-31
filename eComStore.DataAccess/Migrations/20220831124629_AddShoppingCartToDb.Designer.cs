b666f69ef35dd5c09139af1ab5a197a9e494b09f
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-31 15:47:25 +0300

    Section 9 Add ShoppingCart to Database

--- eComStore.DataAccess/Migrations/20220831124629_AddShoppingCartToDb.Designer.cs
+++ eComStore.DataAccess/Migrations/20220831124629_AddShoppingCartToDb.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eComStore.DataAccess.Data;
 
@@ -11,9 +12,10 @@ using eComStore.DataAccess.Data;
 namespace eComStoreWeb.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220831124629_AddShoppingCartToDb")]
+    partial class AddShoppingCartToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
