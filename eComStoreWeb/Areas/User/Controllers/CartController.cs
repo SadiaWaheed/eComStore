bdf31093e77b54e4d1f0f266fde25682a69bd3e0
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-31 19:34:07 +0300

    Section 9 Adding Shopping Cart Display

---
+++ eComStoreWeb/Areas/User/Controllers/CartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eComStore.Web.Areas.User.Controllers
+{
+    [Area("User")]
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
