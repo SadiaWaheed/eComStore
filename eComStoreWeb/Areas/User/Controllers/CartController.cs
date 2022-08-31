5cfc9cacbc92ec49a194ea5003b467bb71ed1e5e
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-08-31 20:45:15 +0300

    Section 9 Updating Quantity in Shopping Cart

--- eComStoreWeb/Areas/User/Controllers/CartController.cs
+++ eComStoreWeb/Areas/User/Controllers/CartController.cs
@@ -34,6 +34,34 @@ namespace eComStore.Web.Areas.User.Controllers
             }
             return View(shoppingCartVM);
         }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(i => i.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(i => i.Id == cartId);
+
+            if (cart.Count <= 1) _unitOfWork.ShoppingCart.Remove(cart);
+            else _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(i => i.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
         private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
         {
             if (quantity <= 50) return price;
