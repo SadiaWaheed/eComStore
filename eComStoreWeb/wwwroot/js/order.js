915dd7826d74d2fb10deaa01235b78abad5062a9
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-09-03 17:10:18 +0300

    Section 11 Display All Orders

---
+++ eComStoreWeb/wwwroot/js/order.js
@@ -0,0 +1,34 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tableData').DataTable({
+        "ajax": {
+            "url": "/Admin/Order/GetAll"
+        },
+        "columns": [
+            { "data": "id", "width": "15%" },
+            { "data": "name", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="w-75 btn-group" role="group">
+                                <a href="/Admin/Order/Details?id=${data}" class="btn btn-primary mx-2">
+                                     <i class="bi bi-pencil-square"></i> &nbsp; Edit
+                                </a>  
+                            </div>
+                           `
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}
