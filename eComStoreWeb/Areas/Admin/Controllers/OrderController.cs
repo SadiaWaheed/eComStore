01c2a4cc61f82ed84f611d67c3495ceff713d377
Sadia Waheed <sadia.waheed312@hotmail.com>
2022-09-05 13:52:02 +0300

    Section 11 Filter orders based on status

--- eComStoreWeb/Areas/Admin/Controllers/OrderController.cs
+++ eComStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using eComStore.DataAccess.Repository.IRepository;
 using eComStore.Model;
+using eComStore.Utility;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eComStore.Web.Areas.Admin.Controllers
@@ -19,10 +20,29 @@ namespace eComStore.Web.Areas.Admin.Controllers
 
         #region API CALLS
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(string status)
         {
             IEnumerable<OrderHeader> orderHeaders;
             orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+                case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+            }
+
             return Json(new { data = orderHeaders });
         }
         #endregion
