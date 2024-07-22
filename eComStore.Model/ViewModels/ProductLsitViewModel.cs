0847f7959a799b58323d011c80713a3a2bdb1e70
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2024-07-22 16:46:02 +0300

    remove second wishlist icon

---
+++ eComStore.Model/ViewModels/ProductLsitViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace eComStore.Model.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ShoppingCart> ListCarts {  get; set; } 
+        public IEnumerable<WishList> WishLists { get; set; }
+    }
+}
