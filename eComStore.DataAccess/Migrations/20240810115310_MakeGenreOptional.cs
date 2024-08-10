94d7dbf78810b8659190a6b1fbbc7614c8528187
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2024-08-10 16:38:25 +0300

    make genre optional

---
+++ eComStore.DataAccess/Migrations/20240810115310_MakeGenreOptional.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eComStoreWeb.Migrations
+{
+    public partial class MakeGenreOptional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Genre_GenreId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Products",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Genre_GenreId",
+                table: "Products",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Genre_GenreId",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GenreId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Genre_GenreId",
+                table: "Products",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
